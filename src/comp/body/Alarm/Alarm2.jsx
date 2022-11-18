69286ab35d2a7b6ef78d90cb68fc5a97636b06fc
sublystrum <petrov082011@mail.ru>
2022-11-19 01:09:08 +0300

    доработки

--- src/comp/body/Alarm/Alarm2.jsx
+++ src/comp/body/Alarm/Alarm2.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react"
+import { CountDown } from "../Timer/Timer"
 import './Alarm.css'
-
 export default function Alarm(){
     const [inputHours, setInputHours] = useState('7')
     const [inputMinuts, setInputMinuts] = useState('40') 
     const [TimeToAlarm, setTimeToAlarm] = useState('')
+    const [shours, setShours] = useState('0')
+    const [sminuts, setSminuts] = useState('0')
+    const [countDownCallState, setCountdownCallState] = useState(false)
 
-    function timeSubmit(){
+    function timeSubmit()
+    {
         let newAlarm = {
             hours: inputHours,
             minuts: inputMinuts
@@ -26,11 +30,11 @@ export default function Alarm(){
                     setTimeToAlarm(calcTime())
                 }
                 changeTimeToAlarm()
-            
+                document.getElementById('closetab').className = 'block'
                 document.getElementById('AlarmM').className = 'block'
                 console.log(TimeToAlarm)
 
-                function calcTime(){
+                function calcTime(state){
                     const a= Number(AlarmHours)*60+Number(AlarmMinuts);
                     const b= Number(RealHours)*60+Number(RealMinuts);
                     let d
@@ -40,30 +44,49 @@ export default function Alarm(){
                     else{
                         d = Math.abs(1440 + a - b)
                     }
-                    const hours=((d/60))                  //сколько часов осталось спать
-                    const minuts= (hours%1)*60           //сколько минут осталось спать
-                   
+                    let hours
+                    function Setthours(){
+                        hours = ((d/60))
+                    return hours                //сколько часов осталось спать
+                    }
+                    
+                    let minuts
+                    function Settminuts() {
+                        minuts= (hours%1)*60    //сколько минут осталось спать
+                        return minuts
+                    }
+                    Setthours()
+                    Settminuts()
+                    setShours(Math.floor(Setthours()))
+                    setSminuts(Math.floor(Settminuts()))
+                    console.log(shours)
+                    console.log(sminuts)                  
+                    setCountdownCallState(true)
                     return (`${(Math.floor(hours))<10 ? '0' + (Math.floor(hours)) : (Math.floor(hours))}:${(Math.floor(minuts))<10 ? '0'+ (Math.floor(minuts)) : (Math.floor(minuts))}`)
                 }
+
             }        
     }
-   
+    function CountDownClose(){
+    setCountdownCallState(false)
+    document.getElementById('closetab').className = 'none'
+    document.getElementById('AlarmM').className = 'none'
+    }
     function poraVstavat(){
         console.log('Пора вставать')
         alert('Пора вставать')
     }
-    
-   
   
-
     return(
         
         <div className='timeInput'>  
             <input className='hoursInput' type="number" name='input1' value={inputHours} onChange={(event)=>setInputHours(event.target.value)} ></input>:
             <input className='minutsInput' type="number" name='input2' value={inputMinuts} onChange={(event)=>setInputMinuts(event.target.value)} ></input>
             <div><input className="submitInput" type="Submit" value="Принять"onClick={timeSubmit} /></div>
-            <div className="none" id="AlarmM">{`Будильник зазвенит через ${TimeToAlarm}` }</div>
-            {/* <div>{`будильник ${h}:${m}`}</div> */}
+            {/* <div className="none" id="AlarmM">{`Будильник зазвенит через ${TimeToAlarm}` }</div> */}
+            <div className="none" id="AlarmM">Будильник зазвенит через</div>
+            { countDownCallState ? <CountDown  hours={shours} minutes={sminuts} /> : null }
+            <div className="rock"><input id="closetab" className='none' type="Submit" value="Удалить"onClick={CountDownClose} /></div>
         </div>
 
     )  
--- src/comp/body/Timer/Timer.jsx
+++ src/comp/body/Timer/Timer.jsx
@@ -1,36 +1,46 @@
-// import React from "react";
-// export const CountDown = (promise) => {
-//     const [over, setOver] = React.useState(false);
-//     const [[h, m, s], setTime] = React.useState([hours, minutes, seconds]);
+import React from "react";
+import './Timer.css';
+export const CountDown = ({ hours = 0, minutes = 0, seconds = 0 }) => {
+    // const [paused, setPaused] = React.useState(false);
+    const [over, setOver] = React.useState(false);
+    const [[h, m, s], setTime] = React.useState([hours, minutes, seconds]);
   
-//     const tick = () => {
-//       if (over) return;
+    const tick = () => {
+      if (over) return;
+      // eslint-disable-next-line eqeqeq
+      if (h === 0 && m === 0 && s === 0) {
+        setOver(true);
+      } else if (m === 0 && s === 0) {
+        setTime([h - 1, 59, 59]);
+      // eslint-disable-next-line eqeqeq
+      } else if (s == 0) {
+        setTime([h, m - 1, 59]);
+      } else {
+        setTime([h, m, s - 1]);
+      }
+    };
   
-//       if (h === 0 && m === 0 && s === 0) {
-//         setOver(true);
-       
-//       } else if (h<0||m<0||s<0){
-//         setOver(true)
-//       } else if (m === 0 && s === 0) {
-//         setTime([h - 1, 59, 59]);
-//       } else if (s === 0) {
-//         setTime([h, m - 1, 59]);
-//       } else {
-//         setTime([h, m, s - 1]);
-//       }
-//     };
-
-//     React.useEffect(() => {
-//       const timerID = setInterval(() => tick(), 1000);
-//       return () => clearInterval(timerID);
-//     });
+    // const reset = () => {
+    //   setTime([parseInt(hours), parseInt(minutes), parseInt(seconds)]);
+    //   setPaused(false);
+    //   setOver(false);
+    // };
   
-//     return (
-//       <div className="none" id="Timer">
-//         <p>{`${h.toString().padStart(2, '0')}:${m
-//           .toString()
-//           .padStart(2, '0')}:${s.toString().padStart(2, '0')}`}</p>
-//         <div>{over ? "Пора вставать" : ''}</div>
-//       </div>
-//     );
-//   };
+    React.useEffect(() => {
+      const timerID = setInterval(() => tick(), 1000);
+      return () => clearInterval(timerID);
+    });
+  
+    return (
+      <div id="CountDowns" className="block">
+        <p>{`${h.toString().padStart(2, '0')}:${m
+          .toString()
+          .padStart(2, '0')}:${s.toString().padStart(2, '0')}`}</p>
+        <div>{over ? "Пора вставати" : ''}</div>
+        {/* <button onClick={() => setPaused(!paused)}>
+          {paused ? 'Resume' : 'Pause'}
+        </button>
+        <button onClick={() => reset()}>Restart</button> */}
+      </div>
+    );
+  };
